6dbc16dac3b58fa7b108f6beb6878d1a9fdbdd18
jehilm <31190173+jehilm@users.noreply.github.com>
2024-12-05 16:49:01 -0500

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, BLOB
 class Image(db.Model):
     __tablename__ = 'images'
 
-    id = Column(Integer, primary_key=True)  # Use review_id as the primary key
+    review_id = Column(Integer, primary_key=True)  # Use review_id as the primary key
     image_data = Column(BLOB, nullable=False)  # Store binary image data
 
     def __repr__(self):
